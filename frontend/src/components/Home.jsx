6bdd6ed54e89ad10942d6ce500a768172e659a6a
Tulasi <tulsishuklag@gmail.com>
2026-04-07 14:38:38 +0530

    frontend/src/components/Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,13 +1,17 @@
+
+
 import React from 'react'
+import Product from './Product'
 
 const Home = () => {
   return (
-    <div className="flex justify-center mt-10">
+    <div className="flex flex-col items-center mt-10">
       <img
         src="https://exclusivemart.netlify.app/static/media/banner1.eba8a127843249448e29.png"
         alt="Shopping Hero"
-        className="w-[70vw] px-14 py-10"
+        className="w-[70vw] px-14 py-10 mx-auto"
       />
+      <Product/>
     </div>
   )
 }
